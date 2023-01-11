766387b3dc77768f2e3a301aa11eab571e9a5eeb
iraafy <irafitriyani62@gmail.com>
2023-01-11 08:27:15 +0700

    upadate

--- resources/views/layouts/landing_page_nav.blade.php
+++ resources/views/layouts/landing_page_nav.blade.php
@@ -6,7 +6,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css' rel='stylesheet'>
     <link rel="stylesheet" href="assets/css/style2.css">
+    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
+    <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
     <!-- Title Page -->
     <title>
         @yield('title')
@@ -17,16 +21,15 @@
     <!-- Start Navbar -->
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-            <div class="container-fluid ps-4 pe-4">
+            <div class="container ps-4 pe-4">
                 <a class="navbar-brand" href="#">
-                    <!-- <img src="assets/img/logo.png" width="70%" alt=""> -->
-                    a
+                    <iconify-icon inline icon="fluent-emoji:robot" class="fs-2"></iconify-icon>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-3">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-5">
                         <li class="nav-item">
                             <a class="nav-link" href="/#about">About Us</a>
                         </li>
