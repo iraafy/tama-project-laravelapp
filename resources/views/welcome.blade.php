3b09b237c6931a6c0548920cafd2425e335c77a3
iraafy <irafitriyani62@gmail.com>
2023-01-11 08:26:42 +0700

    update landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -98,21 +98,23 @@
 <!-- Start Home Section -->
 <section id="home">
     <div class="container-fluid" style="background-image: url(assets/img/header-imgg.png); background-repeat: no-repeat;">
-        <br><br>
-        <div class="row pt-3 pb-5">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <div class="row pt-5 ms-5 ps-5">
-                    <h1>
-                        <h1 class="welcome-text text-color-primary fw-bold lh-1 mt-5">
-                            Tama Hallo
+        <div class="card card-opacity">
+            <br><br>
+            <div class="row pb-5">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="row">
+                        <h1>
+                            <h1 class="welcome-text text-center text-color-primary fw-bold lh-1 mt-5">
+                                Tama Hallo
+                            </h1>
+                            <br>
                         </h1>
-                        <br>
-                    </h1>
-                    <p class="welcome-p-text">Membantu mahasiswa dalam permasalahan</p>
+                        <p class="welcome-p-text text-center">Membantu mahasiswa dalam permasalahan</p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 centered">
-                <img src="assets/img/head.png" width="80%" class="centered" alt="">
+                <div class="col-lg-6 col-md-6 col-sm-12 centered">
+                    <img src="assets/img/head.png" width="80%" class="centered" alt="">
+                </div>
             </div>
         </div>
     </div>
@@ -121,12 +123,12 @@
 
 <!-- Start About Section -->
 <section id="about" class="mb-4">
-    <div class="container-fluid">
-        <div class="row ps-5 pe-5">
-            <div class="col-lg-6 col-md-6 col-sm-12 mb-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-md-6 col-sm-12 mb-3">
                 <img src="assets/img/male.png" width="100%" alt="">
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 centered">
+            <div class="col-lg-7 col-md-6 col-sm-12 centered">
                 <div class="container">
                     <div class="row">
                         <h2 class="right-style-text text-color-primary">
@@ -146,10 +148,9 @@
 <!-- End About Section -->
 
 <!-- Start Requirements Section -->
-<section id="requirements" class="p-5">
-    <br>
+<section id="" class="mt-5 ">
     <div class="container">
-        <div class="row">
+        <div class="row centered">
             <h2 class="mt-3 mb-3 pb-5 text-center text-color-primary">
                 <b>
                     Universitas Pendidikan Indonesia
@@ -168,12 +169,91 @@
         </div>
     </div>
 </section>
+
+<section id="feature" class="pb-5 mt-4">
+    <br><br><br><br>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h3 class="mb-3 fw-bold text-color-primary">Our Feature</h3>
+            </div>
+            <div class="col-6 text-right">
+                <a class="btn bg-color-primary text-white mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-slide="prev">
+                    <i class="fa fa-arrow-left"></i>
+                </a>
+                <a class="btn bg-color-primary text-white mb-3 " href="#carouselExampleIndicators2" role="button" data-slide="next">
+                    <i class="fa fa-arrow-right"></i>
+                </a>
+            </div>
+            <div class="col-12">
+                <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-12 mb-3">
+                                    <a href="/landing_topic">
+                                        <div class="card card-hover shadow p-5">
+                                            <img style="width: 51%" class="centered" alt="100%x280" src="assets/img/skripsi.png">
+                                            <div class="card-body text-center pt-4">
+                                                <h4 class="card-title text-color-primary">Topik Skripsi</h4>
+                                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-md-6 col-sm-12 mb-3">
+                                    <a href="/interns">
+                                        <div class="card card-hover shadow p-5">
+                                            <img style="width: 40%" class="centered" alt="100%x280" src="assets/img/ppl.png">
+                                            <div class="card-body text-center pt-4">
+                                                <h4 class="card-title text-color-primary">Rekomendasi PPL</h4>
+                                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-12 mb-3">
+                                    <a href="/forums">
+                                        <div class="card card-hover shadow p-5">
+                                            <img style="width: 40%" class="centered" alt="100%x280" src="assets/img/forum.png">
+                                            <div class="card-body text-center pt-4">
+                                                <h4 class="card-title text-color-primary">Forum Diskusi</h4>
+                                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-md-6 col-sm-12 mb-3">
+                                    <a href="https://ridzky.itch.io/tesproject">
+                                        <div class="card card-hover shadow p-5">
+                                            <img style="width: 40%" class="centered" alt="100%x280" src="assets/img/denah.png">
+                                            <div class="card-body text-center pt-4">
+                                                <h4 class="card-title text-color-primary">Denah UPI</h4>
+                                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <!-- End Requirements Section -->
 
 <!-- Start Footer -->
-<div class="footer">
-    Copyright all reserved &copy; 2022
+<div class="footer mt-5">
+    Copyright &copy; 2023 <a href="/">Tama Project Team</a>. All Rights Reserved.
 </div>
 <!-- End Footer -->
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 @endsection
